b0469453ac121b1ba7d5256066a663bc959e800a
Nain Luna <nainlunini@gmail.com>
2026-07-09 09:53:24 -0700

    fix: prevent layout unmounting on route change to fix footer flash, and add scrollbar-gutter to prevent layout shift

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -13,51 +13,47 @@ import { authProtectedRoutes, publicRoutes } from "./allRoutes";
 import AuthProtected  from './AuthProtected';
 
 const Index = () => {
-    const location = useLocation();
-
     return (
         <React.Fragment>
-            <AnimatePresence mode="wait">
-                <Routes location={location} key={location.pathname}>
-                    <Route>
-                        {publicRoutes.map((route, idx) => (
-                            <Route
-                                path={route.path}
-                                element={
-                                    <PageTransition>
-                                        <NonAuthLayout>
-                                            <Suspense fallback={<div className="d-flex justify-content-center mx-2 mt-2"><Spinner color="primary">Cargando...</Spinner></div>}>
+            <Routes>
+                <Route>
+                    {publicRoutes.map((route, idx) => (
+                        <Route
+                            path={route.path}
+                            element={
+                                <NonAuthLayout>
+                                    <PageTransition key={route.path}>
+                                        <Suspense fallback={<div className="d-flex justify-content-center mx-2 mt-2"><Spinner color="primary">Cargando...</Spinner></div>}>
+                                            {route.component}
+                                        </Suspense>
+                                    </PageTransition>
+                                </NonAuthLayout>
+                            }
+                            key={idx}
+                        />
+                    ))}
+                </Route>
+
+                <Route>
+                    {authProtectedRoutes.map((route, idx) => (
+                        <Route
+                            path={route.path}
+                            element={
+                                <AuthProtected>
+                                    <VerticalLayout>
+                                        <PageTransition key={route.path}>
+                                            <Suspense fallback={<div className="page-content"><div className="d-flex justify-content-center mx-2 mt-2"><Spinner color="primary">Cargando...</Spinner></div></div>}>
                                                 {route.component}
                                             </Suspense>
-                                        </NonAuthLayout>
-                                    </PageTransition>
-                                }
-                                key={idx}
-                            />
-                        ))}
-                    </Route>
-
-                    <Route>
-                        {authProtectedRoutes.map((route, idx) => (
-                            <Route
-                                path={route.path}
-                                element={
-                                    <AuthProtected>
-                                        <PageTransition>
-                                            <VerticalLayout>
-                                                <Suspense fallback={<div className="page-content"><div className="d-flex justify-content-center mx-2 mt-2"><Spinner color="primary">Cargando...</Spinner></div></div>}>
-                                                    {route.component}
-                                                </Suspense>
-                                            </VerticalLayout>
                                         </PageTransition>
-                                    </AuthProtected>
-                                }
-                                key={idx}
-                            />
-                        ))}
-                    </Route>
-                </Routes>
-            </AnimatePresence>
+                                    </VerticalLayout>
+                                </AuthProtected>
+                            }
+                            key={idx}
+                        />
+                    ))}
+                </Route>
+            </Routes>
         </React.Fragment>
     );
 };
